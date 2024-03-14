330c468a8f83a1a8eb30163aabfcf0a1ec771cf8
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-14 14:15:21 -0400

    Initialize that thing [index.py]

--- index.py
+++ index.py
@@ -1,3 +1,17 @@
 from flask import Flask, redirect,url_for,render_template                                                                                                               ## Import the flask library
 import os                                                                                                                                                               ## Import the standard OS library
 import json                                                                                                                                                             ## Import the json library
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
