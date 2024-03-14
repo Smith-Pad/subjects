d8a560ae548c2d56afa64014221f569e34268c89
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-14 13:04:07 -0400

    Imported the json library [index.py]

--- index.py
+++ index.py
@@ -1,2 +1,3 @@
 from flask import Flask, redirect,url_for,render_template                                                                                                               ## Import the flask library
 import os                                                                                                                                                               ## Import the standard OS library
+import json                                                                                                                                                             ## Import the json library
