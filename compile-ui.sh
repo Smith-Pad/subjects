9b151c20d41327a13dc2c12a1733e53135ee877f
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-24 00:44:30 -0400

    mmm

--- compile-ui.sh
+++ compile-ui.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+## compile-ui.sh
 
-# Remove the old index.css file
-rm -f static/index.css
 
-# Loop through all .scss files in the UI directory and append their contents to index-new.css
-for file in UI/*.scss
-do
-  sassc "$file" >> static/index-new.css
-done
+function addFile() {
+        for file in UI/*.scss
+        do
+                sassc "$file" >> static/index-new.css
+        done
+}
+
+addFile
