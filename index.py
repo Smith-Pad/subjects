c3a146cb7a0119e21ff971efb9ab16c98ee71394
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-25 00:07:47 -0700

    Senal has to format the code isn't it...

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect,url_for,render_template                                                                                                               ## Import the flask library
-import os                                                                                                                                                               ## Import the standard OS library
-import json                                                                                                                                                             ## Import the json library
+from flask import Flask, redirect,url_for,render_template                       ## Import the flask library
+import os                                                                       ## Import the standard OS library
+import json                                                                     ## Import the json library
 
 
 
