5b8f3b6e2d6bb75148c78b606533d6b598cb2b63
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-27 11:18:06 -0400

    mmm

---
+++ topic_routes.py
@@ -0,0 +1,24 @@
+'''
+topic_routes.py
+
+This particular implementation of routes for the subjects topic section 
+of the Subjects feature of Smith-Pad
+'''
+
+
+from flask import Blueprint, render_template
+
+
+## Initialize the routing system blueprint for the subjects route
+routingsystem = Blueprint('routingsystem', __name__)
+
+
+
+'''
+This is where the routes will dynamically be generated here. For now, 
+for the Pliot II, this will be manually generated. 
+
+'''
+@routingsystem.route('/utils_route')
+def utils_route():
+    return render_template('testing-view.html')
