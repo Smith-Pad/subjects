0f2c71daa00ee29d885f91fb0635e317055989b9
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-27 14:26:08 -0400

    mmm

--- topic_routes.sh
+++ topic_routes.sh
@@ -1,2 +1,6 @@
 ## topic-routes.sh 
-tail -c 25 "topic_routes.py" && echo "\n" >> topic_routes.py
+tail -c 25 "topic_routes.py" && echo "\n" >> topic_routes.py 
+
+echo """
+testing
+""" >> topic_routes.py
