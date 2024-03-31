3bec4bd979a15500fad563886a15f32f8fc26490
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-31 16:10:59 -0400

    mmm

--- index.py
+++ index.py
@@ -19,6 +19,45 @@ def main_menu():
         return render_template('main_menu.html')
 
 
+"""
+Sample Lessons are here
+"""
+
+@app.route("/sample_lesson_001")
+def sample_lesson_001():
+        return render_template('_Lesson-Template_/index.html')
+
+
+@app.route("/sample_lesson_002")
+def sample_lesson_002():
+        return render_template('_Lesson-Template_/game_menu.html')
+
+
+
+@app.route("/sample_lesson_003")
+def sample_lesson_003():
+        return render_template('_Lesson-Template_/introduction.html')
+
+
+
+@app.route("/sample_lesson_004")
+def sample_lesson_004():
+        return render_template('_Lesson-Template_/01.html')
+
+
+
+@app.route("/sample_lesson_005")
+def sample_lesson_005():
+        return render_template('_Lesson-Template_/02.html')
+
+
+
+@app.route("/sample_lesson_006")
+def sample_lesson_006():
+        return render_template('_Lesson-Template_/03.html')
+
+
+
 app.register_blueprint(routingsystem)
 
 
