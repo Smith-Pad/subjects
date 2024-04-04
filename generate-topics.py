9b9fb24e2f54a85fce8bf24b649be6528579a6d9
senalbulumulle <senal.bulumulle@gmail.com>
2024-04-04 11:42:22 -0400

    mmm

--- generate-topics.py
+++ generate-topics.py
@@ -10,16 +10,20 @@ import os                                                                       
 Global Variables
 ----------------
 '''
-lesson_title        = 0
-lesson_title_file   = 0
 
+lesson_title                            = 0
+lesson_title_file                       = 0
+
+lesson_title_find                       = 0
+lesson_title_replace                    = 0
 
 os.system("clear")
 lesson_title = input("Enter the title of the lesson:\n\n")
 os.system("echo '" + lesson_title  + "'")
+os.system("sed -i '' 's/hello/" + lesson_title + "/g' testing.html")
 
 
 
 os.system("clear")
 lesson_title_file = input("Enter the title of lesson file\n\n")
-os.system("echo '" + lesson_title_file + "'")
+os.system("sed -i '' 's/hello/" + lesson_title_file + "/g' testing.html")
