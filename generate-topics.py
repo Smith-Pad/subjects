4d429ae47b6d972a12ead3ed628221464d98c814
senalbulumulle <senal.bulumulle@gmail.com>
2024-04-04 10:34:25 -0400

    mmm

--- generate-topics.py
+++ generate-topics.py
@@ -15,9 +15,10 @@ lesson_title_file   = 0
 
 
 os.system("clear")
-
 lesson_title = input("Enter the title of the lesson:\n\n")
 
-os.system("clear")
 
+
+
+os.system("clear")
 lesson_title_file = input("Enter the title of lesson file\n\n")
