7cdd28d27de9cbf6a1bd461658fe98bb64e0f314
senalbulumulle <senal.bulumulle@gmail.com>
2024-04-04 12:27:30 -0400

    mmm

--- generate-topics.py
+++ generate-topics.py
@@ -18,10 +18,10 @@ lesson_title_file                       = 0
 os.system("clear")
 lesson_title = input("Enter the title of the lesson:\n\n")
 os.system("echo '" + lesson_title  + "'")
-os.system("sed -i '' 's/hello/" + lesson_title + "/g' testing.html")
+os.system("sed -i '' 's/<h1>hello</h1>/" + lesson_title + "/g' testing.html")
 
 
 
 os.system("clear")
 lesson_title_file = input("Enter the title of lesson file\n\n")
-os.system("sed -i '' 's/hello/" + lesson_title_file + "/g' testing.html")
+os.system("sed -i '' 's/<h1>hello<\/h1>/" + "<h1>" + lesson_title_file + "<\/h1>/g' testing.html")
