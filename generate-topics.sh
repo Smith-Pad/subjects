c72ff3046f3ffb7fe2b7a856b6b5c03bae8aab60
senalbulumulle <senal.bulumulle@gmail.com>
2024-04-14 11:22:14 -0400

    mmm

---
+++ generate-topics.sh
@@ -0,0 +1,15 @@
+## generate-topics.sh 
+
+
+
+
+## In this function, we are calculating the 2+2 function 
+## which creates a new line 
+new_line=$(expr 2 + 30)          
+echo "$new_line"
+
+
+
+sed -n "${new_line}p" topic_routes.py
+
+
