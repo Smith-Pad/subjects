7cc29a7e2a15f3d7fbb7d12b15e9ac7e353ebe15
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-04 02:53:59 +0000

    --> <-- route

--- index.py
+++ index.py
@@ -60,5 +60,11 @@ def game_ui_test_05():
 
 
 
+@app.route('/game_ui_test_06')
+def game_ui_test_05():
+        return render_template('template-new-refreshed/06.html')
+
+
+
 if __name__ == '__main__':
         app.run(debug=True)
