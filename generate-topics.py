322545ff8146eadd04c0bc4d0b6d170c020f749f
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-13 11:27:45 -0400

    oh

--- generate-topics.py
+++ generate-topics.py
@@ -18,3 +18,12 @@ $$$$$$$$\ $$\   $$\     $$\
 ''')
 
 lesson_title = input("Input a Lesson Title Here\n\n\n---\n\n----\t\t")
+
+
+with open('topic_routes.py', 'a') as file:
+    file.write('\n')  
+    file.write('@app.route("/")\n')
+    file.write('@app.route(')')
+    file.write('def index():\n')
+    file.write('\t\treturn render_template("hello world")')
+    file.write('\n\n\n')
