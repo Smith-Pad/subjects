13edc3e232465fb9a1395bc8ef53807dc3e7d123
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-14 14:52:30 -0400

    oh

--- generate-topics.py
+++ generate-topics.py
@@ -7,23 +7,21 @@ MODEL_TYPE = "gemma"
 
 lesson_title = input("Input a Lesson Title Here:")
 
-with open('topic_routes.py', 'a') as file:
-    file.write('\n')  
-    file.write('@app.route("/' + lesson_title + '")\n')
-    file.write('def index():\n')
-    file.write('\treturn render_template("hello.html")\n')
-    file.write('\n')
+# with open('topic_routes.py', 'a') as file:
+#     file.write('\n')  
+#     file.write('@app.route("/' + lesson_title + '")\n')
+#     file.write('def index():\n')
+#     file.write('\treturn render_template("hello.html")\n')
+#     file.write('\n')
 
 
 
+# response = ollama.chat(model=MODEL_TYPE, messages=[
+#   {
+#     'role': 'user',
+#     'content': 'Can you give me an introductionary sentence about ' + lesson_title + '',
+#   },
+# ])
 
-response = ollama.chat(model=MODEL_TYPE, messages=[
-  {
-    'role': 'user',
-    'content': 'Can you give me an introductionary sentence about ' + lesson_title + '',
-  },
-])
 
-
-print(response['message']['content']) 
-
+# print(response['message']['content']) 
