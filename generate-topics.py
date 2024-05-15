770bcec59364db029335901ae56ba1bda463a968
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-15 10:25:30 -0400

    LET'S GOOOO

--- generate-topics.py
+++ generate-topics.py
@@ -5,17 +5,11 @@ import shutil
 
 
 
-# lesson_title = input("Input a Lesson Title Here: ")
+lesson_title = input("Input a Lesson Title Here: ")
 
 # template_dir = "templates/template-new-refreshed"
-
-
 # new_dir = "templates/" + lesson_title
-
-
 # shutil.copytree(template_dir, new_dir)
-
-
 # os.rename(new_dir, "templates/" + lesson_title)
 
 
@@ -37,13 +31,80 @@ import shutil
 
 
 
+########################################################################
+#                   Training Methods
+#
+#           
+########################################################################
 
 
+########################################################################
+# Subject Generation Levels           
+########################################################################
+
+# LEVEL_1 = "true"
+# LEVEL_2 = "true"
+# LEVEL_3 = "true"
+# LEVEL_4 = "true"
+# LEVEL_5 = "true"
 
 
+################################################################
+##   GLOBAL TRAINING VARIABLES
+################################################################
+
+MODEL_TYPE = "gemma"
+MAIN_MENU_WIDGET_BUTTON_DESCRIPTION_QUESTION = "Can you give me an summary sentence on the "
+LEVEL_1 = "Explain like I'm a five year old learning how to" + MAIN_MENU_WIDGET_BUTTON_DESCRIPTION_QUESTION
+
+
+response = ollama.chat(model=MODEL_TYPE, messages=[
+  {
+    'role': 'user',
+    'content': MAIN_MENU_WIDGET_BUTTON_DESCRIPTION_QUESTION + lesson_title + LEVEL_1,
+  },
+])
+
+output_html = response['message']['content']
 
 ## This generates a widget button for the main_menu.html for the topic
-os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"><div class=\"widget-boxes-header-title\"><h1> Hello world </h1><div class=\"widget-boxes-header-description\"><h2> Blah blah blah </h2></div></div></div> }g' templates/main_menu.html")
+os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"><div class=\"widget-boxes-header-title\"><h1>" + lesson_title + "</h1><div class=\"widget-boxes-header-description\">" + str(output_html)  + "<h2> Blah blah blah </h2></div></div></div> \n\n generate starts here}g' templates/main_menu.html")
+
+
+
+
+
+
+
+
+
+
+
+################################################################
+##              NOT NEEDED
+################################################################
+
+
+
+# lesson_title_file = input("Enter the title of lesson file\n\n")
+# os.system("sed -i '' 's/<h1>hello<\/h1>/" + "<h1>" + lesson_title_file + "<\/h1>/g' testing.html")
+
+
+
+# MODEL_TYPE = "gemma"
+
+# response = ollama.chat(model=MODEL_TYPE, messages=[
+#   {
+#     'role': 'user',
+#     'content': 'Can you give me an summary sentence on the ' + lesson_title + '',
+#   },
+# ])
+
+# output_html = print(response['message']['content']) 
+# print(output_html)
+# os.system("touch hello.txt")
+# os.system("echo '" + output_html + "' >> hello.txt")
+# os.system("sed -i '' \"s/generate starts here./generate starts now/g\" templates/main_menu.html")
 
 
 
@@ -59,27 +120,3 @@ os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"
 #     file.write('def index():\n')
 #     file.write('\treturn render_template("hello.html")\n')
 #     file.write('\n')
-
-
-
-# response = ollama.chat(model=MODEL_TYPE, messages=[
-#   {
-#     'role': 'user',
-#     'content': 'Can you give me an introductionary sentence about ' + lesson_title + '',
-#   },
-# ])
-
-
-# print(response['message']['content']) 
-
-
-
-################################################################
-##              NOT NEEDED
-################################################################
-
-
-
-# lesson_title_file = input("Enter the title of lesson file\n\n")
-# os.system("sed -i '' 's/<h1>hello<\/h1>/" + "<h1>" + lesson_title_file + "<\/h1>/g' testing.html")
-# os.system("sed -i '' \"s/generate starts here./generate starts now/g\" templates/main_menu.html")
