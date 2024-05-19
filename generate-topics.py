4008073b0eb3c779852a5ebd8b037786690e3df0
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-19 14:34:50 -0400

    ye

--- generate-topics.py
+++ generate-topics.py
@@ -18,18 +18,17 @@ This is where you are able to change the type of model here.
 MODEL_TYPE = "gemma"
 
 
-response = ollama.chat(model=MODEL_TYPE, messages=[
+widget_menu_response = ollama.chat(model=MODEL_TYPE, messages=[
   {
     'role': 'user',
     'content': "Can you give me a summary sentence on the" + lesson_title +  "Explain like I'm a five year old learning how to"
   },
 ])
 
-output_html = response['message']['content']
+output_html = widget_menu_response['message']['content']
 
-## This generates a widget button for the main_menu.html for the topic
+
 os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"><div class=\"widget-boxes-header-title\"><h1>" + lesson_title + "</h1><div class=\"widget-boxes-header-description\">" + str(output_html)  + "<h2> Blah blah blah </h2> <button class=''osui-button''>lesson title</button></div></div></div> \n\n generate starts here}g' templates/main_menu.html")
-
 
 '''
 @docs 
