40f836881cbad797eb01083426e0adab1717bbe1
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-19 17:31:32 -0400

    ye

--- generate-topics.py
+++ generate-topics.py
@@ -3,77 +3,22 @@ import ollama
 import os
 import shutil
 
-
-
+# Prompt user to input lesson title
 lesson_title = input("Input a Lesson Title Here: ")
 
-'''
-------
-@docs
-------
+# Model type for widget generation
+model_type = "gemma"
 
-This is where you are able to change the type of model here. 
-'''
+# Chat with Ollama using specified model type
+widget_menu_response = ollama.chat(model=model_type, messages=[{'role': 'user','content': "Can you give me a summary sentence on the " + lesson_title + "? Explain like I'm a five year old learning how to." },])
 
-WIDGET_MENU_BUTTON_MODEL_TYPE = "gemma"
-TOPIC_MENU_BUTTON_MODEL_TYPE = "gemma"
+# Extract HTML content from widget menu response
+widget_menu_response_output = widget_menu_response['message']['content']
 
+# Update HTML file with lesson title and content
+os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"><div class=\"widget-boxes-header-title\"><h1>" + lesson_title + "</h1><div class=\"widget-boxes-header-description\">" + str(widget_menu_response_output)  + "<h2> Blah blah blah </h2> <button class=''osui-button''>lesson title</button></div></div></div> \n\n generate starts here}g' templates/main_menu.html")
 
-widget_menu_response = ollama.chat(model=WIDGET_MENU_BUTTON_MODEL_TYPE, messages=[
-  {
-    'role': 'user',
-    'content': "Can you give me a summary sentence on the" + lesson_title +  "Explain like I'm a five year old learning how to"
-  },
-])
-
-output_html = widget_menu_response['message']['content']
-
-
-os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"><div class=\"widget-boxes-header-title\"><h1>" + lesson_title + "</h1><div class=\"widget-boxes-header-description\">" + str(output_html)  + "<h2> Blah blah blah </h2> <button class=''osui-button''>lesson title</button></div></div></div> \n\n generate starts here}g' templates/main_menu.html")
-
-
-'''
-------
-@docs
-------
-This may be used for easier maintainability 
-s = FIND
-g = REPLACE
-
-
-
-'''
-
-# os.system("perl -pi -e 's{generate starts here}{"
-#           "<div class=\"widget-boxes-init\">"
-#           "<div class=\"widget-boxes-header-title\">"
-#           "<h1>" + lesson_title + "</h1>"
-#           "<div class=\"widget-boxes-header-description\">" 
-#           + str(output_html) + "<h2> Blah blah blah </h2> "
-#           "<button class=''osui-button''>lesson title</button>"
-#           "</div></div></div> \n\n generate starts here}g' templates/main_menu.html")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-@docs 
--------
-This allows the ability to copy the template-new-refreshed to the 
-specified subject generation. 
-'''
-
+# Copy template directory and rename it with lesson title
 template_dir = "templates/template-new-refreshed"
 new_dir = "templates/" + lesson_title
 shutil.copytree(template_dir, new_dir)
