07e3adc20b50ab91a94b2f9d40d6563ba3c8ef0d
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-19 17:24:56 -0400

    ye

--- generate-topics-again.py
+++ generate-topics-again.py
@@ -1,39 +1,37 @@
 import ollama
+import os
 import shutil
-import subprocess
 
-# Input lesson title
+# Prompt user to input lesson title
 lesson_title = input("Input a Lesson Title Here: ")
 
-# Prompt for a summary sentence
-summary_prompt = f"Can you give me a summary sentence on the {lesson_title}? Explain like I'm a five year old learning how to"
-widget_menu_response = ollama.chat(model="gemma", messages=[{"role": "user", "content": summary_prompt}])
+'''
+------
+@docs
+------
+
+This is where you are able to change the type of model here. 
+'''
+
+# Model type for widget generation
+model_type = "gemma"
+
+# Chat with Ollama using specified model type
+widget_menu_response = ollama.chat(model=model_type, messages=[
+  {
+    'role': 'user',
+    'content': "Can you give me a summary sentence on the " + lesson_title + "? Explain like I'm a five year old learning how to."
+  },
+])
+
+# Extract HTML content from widget menu response
 output_html = widget_menu_response['message']['content']
 
-# Update HTML template
-html_template = f'''
-<div class="widget-boxes-init">
-    <div class="widget-boxes-header-title">
-        <h1>{lesson_title}</h1>
-        <div class="widget-boxes-header-description">
-            {output_html}
-            <h2> Blah blah blah </h2>
-            <button class="osui-button">{lesson_title}</button>
-        </div>
-    </div>
-</div>
-'''
-template_file = "templates/main_menu.html"
-with open(template_file, "r+") as f:
-    content = f.read()
-    f.seek(0, 0)
-    f.write(html_template + "\n\n" + content)
+# Update HTML file with lesson title and content
+os.system("perl -pi -e 's{generate starts here}{<div class=\"widget-boxes-init\"><div class=\"widget-boxes-header-title\"><h1>" + lesson_title + "</h1><div class=\"widget-boxes-header-description\">" + str(output_html)  + "<h2> Blah blah blah </h2> <button class=''osui-button''>lesson title</button></div></div></div> \n\n generate starts here}g' templates/main_menu.html")
 
-# Copy template directory
+# Copy template directory and rename it with lesson title
 template_dir = "templates/template-new-refreshed"
-new_dir = f"templates/{lesson_title}"
-try:
-    shutil.copytree(template_dir, new_dir)
-    os.rename(new_dir, f"templates/{lesson_title}")
-except Exception as e:
-    print("Error:", e)
+new_dir = "templates/" + lesson_title
+shutil.copytree(template_dir, new_dir)
+os.rename(new_dir, "templates/" + lesson_title)
