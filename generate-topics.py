55acca07cccea8afff8d01bdd1b35d4a8d480437
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-20 15:17:25 -0400

    ye

--- generate-topics.py
+++ generate-topics.py
@@ -4,15 +4,17 @@
 
 import os 
 import shutil
+from ollama import generate
 
+## Create dir
 
 os.system("cp -R templates/template-new-refreshed templates/abouttorename")
-
 lesson_title = input("Enter the topic name: ")
 lesson_title = lesson_title.replace(" ", "_")
-
 os.system(f"mv templates/abouttorename templates/{lesson_title}")
 
 
-## This is for the first page
-os.system(f"perl -pi -e 's/content generation starts here/hello words/g' templates/{lesson_title}/01.html")
+response = generate('gemma', 'Why is the sky blue? in a short sentence')['response']
+print(response)
+
+os.system(f"perl -pi -e 's/content generation starts here/{response}/g' templates/{lesson_title}/01.html")
