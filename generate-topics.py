50c43d573fff4d26183decb2799d7ad9282ed135
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-22 13:04:48 -0400

    ye

--- generate-topics.py
+++ generate-topics.py
@@ -7,21 +7,19 @@ import shutil
 from ollama import generate
 
 
-def createDir():
-    ## Create dir
-    # os.system("cp -R templates/template-new-refreshed templates/abouttorename")
-    # lesson_title = input("Enter the topic name: ")
-    # lesson_title = lesson_title.replace(" ", "_")
-    # os.system(f"mv templates/abouttorename templates/{lesson_title}")
 
 
-def askQuestion():
-    firstOne = generate('gemma', 'Can you summarize the alphabet song for kids under 5 sentences?')['response']
-    print(firstOne + "\n\n\n")
- 
-createDir()
-askQuestion()
+os.system("cp -R templates/template-new-refreshed templates/abouttorename")
+lesson_title = input("Enter the topic name: ")
+lesson_title = lesson_title.replace(" ", "_")
+os.system(f"mv templates/abouttorename templates/{lesson_title}")
 
 
 
-# os.system(f"perl -pi -e 's/content generation starts here/{response}/g' templates/{lesson_title}/01.html")
+firstOne = generate('gemma', 'Can you summarize the alphabet song for kids under 5 sentences?')['response']
+print(firstOne + "\n\n\n")
+
+
+
+
+os.system(f"perl -pi -e 's/content generation starts here/{response}/g' templates/{lesson_title}/01.html")
