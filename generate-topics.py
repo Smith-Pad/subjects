95d36a60f59258dd9fd3c02abddefa0b2874a9e3
senalbulumulle <senal.bulumulle@gmail.com>
2024-05-31 18:14:03 -0400

    I found a light

--- generate-topics.py
+++ generate-topics.py
@@ -69,33 +69,41 @@ print(''')
 os.system("sleep 5")
 os.system("clear")
 
-os.system("cp -R templates/template-new-refreshed templates/abouttorename")
+# os.system("cp -R templates/template-new-refreshed templates/abouttorename")
 
-lesson_title = input('''
-# ███████╗███╗░░██╗████████╗███████╗██████╗░  ████████╗██╗████████╗██╗░░░░░███████╗
-# ██╔════╝████╗░██║╚══██╔══╝██╔════╝██╔══██╗  ╚══██╔══╝██║╚══██╔══╝██║░░░░░██╔════╝
-# █████╗░░██╔██╗██║░░░██║░░░█████╗░░██████╔╝  ░░░██║░░░██║░░░██║░░░██║░░░░░█████╗░░
-# ██╔══╝░░██║╚████║░░░██║░░░██╔══╝░░██╔══██╗  ░░░██║░░░██║░░░██║░░░██║░░░░░██╔══╝░░
-# ███████╗██║░╚███║░░░██║░░░███████╗██║░░██║  ░░░██║░░░██║░░░██║░░░███████╗███████╗
-# ╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ░░░╚═╝░░░╚═╝░░░╚═╝░░░╚══════╝╚══════╝
+# lesson_title = input('''
+# # ███████╗███╗░░██╗████████╗███████╗██████╗░  ████████╗██╗████████╗██╗░░░░░███████╗
+# # ██╔════╝████╗░██║╚══██╔══╝██╔════╝██╔══██╗  ╚══██╔══╝██║╚══██╔══╝██║░░░░░██╔════╝
+# # █████╗░░██╔██╗██║░░░██║░░░█████╗░░██████╔╝  ░░░██║░░░██║░░░██║░░░██║░░░░░█████╗░░
+# # ██╔══╝░░██║╚████║░░░██║░░░██╔══╝░░██╔══██╗  ░░░██║░░░██║░░░██║░░░██║░░░░░██╔══╝░░
+# # ███████╗██║░╚███║░░░██║░░░███████╗██║░░██║  ░░░██║░░░██║░░░██║░░░███████╗███████╗
+# # ╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ░░░╚═╝░░░╚═╝░░░╚═╝░░░╚══════╝╚══════╝
 
-# ██╗░░██╗███████╗██████╗░███████╗
-# ██║░░██║██╔════╝██╔══██╗██╔════╝
-# ███████║█████╗░░██████╔╝█████╗░░
-# ██╔══██║██╔══╝░░██╔══██╗██╔══╝░░
-# ██║░░██║███████╗██║░░██║███████╗
-# ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚══════╝
-''')
-
-
-lesson_title = lesson_title.replace(" ", "_")
+# # ██╗░░██╗███████╗██████╗░███████╗
+# # ██║░░██║██╔════╝██╔══██╗██╔════╝
+# # ███████║█████╗░░██████╔╝█████╗░░
+# # ██╔══██║██╔══╝░░██╔══██╗██╔══╝░░
+# # ██║░░██║███████╗██║░░██║███████╗
+# # ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚══════╝
+# ''')
 
 
-os.system(f"mv templates/abouttorename templates/{lesson_title}")
+# lesson_title = lesson_title.replace(" ", "_")
 
 
-os.system(f"perl -pi -e 's/<!-- generate starts here -->/<button class=\"FOIL-button-regular color-is-blue\">" + lesson_title + "<\/button>\\n\\n\\n <!-- generate starts here -->/g' templates/main_menu.html")
+# os.system(f"mv templates/abouttorename templates/{lesson_title}")
 
 
-firstOne = generate('gemma', 'Can you summarize the alphabet song for kids under 5 sentences?')['response']
-print(firstOne + "\n\n\n")
+# os.system(f"perl -pi -e 's/<!-- generate starts here -->/<button class=\"FOIL-button-regular color-is-blue\">" + lesson_title + "<\/button>\\n\\n\\n <!-- generate starts here -->/g' templates/main_menu.html")
+
+
+# firstOne = generate('gemma', 'Can you summarize the alphabet song for kids under 5 sentences?')['response']
+# print(firstOne + "\n\n\n")
+
+
+
+
+
+
+
+
