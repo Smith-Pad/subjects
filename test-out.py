1071e124ea0f7a978a772ef7fc4f00f7a4beb1ad
senalbulumulle <senal.bulumulle@gmail.com>
2024-06-09 17:50:25 -0400

    refactor is fun...

--- test-out.py
+++ test-out.py
@@ -11,86 +11,83 @@ route_replacer_enabled = True
 
 
 
-def configure_routes():
-    lesson_title = input("Input title: ")
-    lesson_title = lesson_title.replace(" ", "_")
-    route_search_text = "## generate starts here"
 
-    route_replace_text = f"""
+lesson_title = input("Input title: ")
+lesson_title = lesson_title.replace(" ", "_")
+route_search_text = "## generate starts here"
+
+route_replace_text = f"""
 ## Splash Screen Generated
 
 @app.route('/{lesson_title}')
 def {lesson_title}():
-    return '''
-    <link rel="stylesheet" href="/static/GENERATE.css">
-    <h1> hello world </h1>
-    <h2> hello world </h2>
-    <h3> hello world </h3>
-    <h4> hello world </h4>
-    <h5> hello world </h5>
-    '''
+return '''
+<link rel="stylesheet" href="/static/GENERATE.css">
+<h1> hello world </h1>
+<h2> hello world </h2>
+<h3> hello world </h3>
+<h4> hello world </h4>
+<h5> hello world </h5>
+'''
 
 
 
 @app.route('/{lesson_title}_begin')
 def {lesson_title}_begin():
-    return '''
-    <link rel="stylesheet" href="/static/GENERATE.css">
-    <h1> hello world </h1>
-    <h2> hello world </h2>
-    <h3> hello world </h3>
-    <h4> hello world </h4>
-    <h5> hello world </h5>
-    '''
+return '''
+<link rel="stylesheet" href="/static/GENERATE.css">
+<h1> hello world </h1>
+<h2> hello world </h2>
+<h3> hello world </h3>
+<h4> hello world </h4>
+<h5> hello world </h5>
+'''
 
 
 
 
 @app.route('/{lesson_title}_01')
 def {lesson_title}_01():
-    return '''
-    <link rel="stylesheet" href="/static/GENERATE.css">
-    <h1> hello world </h1>
-    <h2> hello world </h2>
-    <h3> hello world </h3>
-    <h4> hello world </h4>
-    <h5> hello world </h5>
-    '''
+return '''
+<link rel="stylesheet" href="/static/GENERATE.css">
+<h1> hello world </h1>
+<h2> hello world </h2>
+<h3> hello world </h3>
+<h4> hello world </h4>
+<h5> hello world </h5>
+'''
 
 @app.route('/{lesson_title}_02')
 def {lesson_title}_02():
-    return '''
-    <link rel="stylesheet" href="/static/GENERATE.css">
-    <h1> hello world </h1>
-    <h2> hello world </h2>
-    <h3> hello world </h3>
-    <h4> hello world </h4>
-    <h5> hello world </h5>
-    '''
+return '''
+<link rel="stylesheet" href="/static/GENERATE.css">
+<h1> hello world </h1>
+<h2> hello world </h2>
+<h3> hello world </h3>
+<h4> hello world </h4>
+<h5> hello world </h5>
+'''
 
 
 @app.route('/{lesson_title}_03')
 def {lesson_title}_03():
-    return "2341"
+return "2341"
 
 
 
 @app.route('/{lesson_title}_04')
 def {lesson_title}_04():
-    return "231"
+return "231"
 
 
 
 @app.route('/{lesson_title}_05')
 def {lesson_title}_05():
-    return "1"
-                
+return "1"
+            
 """ + "\n\n\n\n## generate starts here"
-    
-    with open('index.py', 'r+') as file:
-        content = file.read().replace(route_search_text, route_replace_text)
-        file.seek(0)  
-        file.write(content)
 
-
-configure_routes()
+with open('index.py', 'r+') as file:
+    content = file.read().replace(route_search_text, route_replace_text)
+    file.seek(0)  
+    file.write(content)
