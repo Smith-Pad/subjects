86eaa969874d67a3cba44417e56d5b6d92e4be7f
senalbulumulle <senal.bulumulle@gmail.com>
2024-08-09 10:21:48 -0400

    Added variable for the description test on the [generation.test.add.latest.assignments.cards.py

--- _generation.test.add.latest.assignments.cards.py
+++ _generation.test.add.latest.assignments.cards.py
@@ -3,7 +3,7 @@ import os
 
 GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE = "./templates/_latest.assignments.cards.html"
 GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_TITLE = "Hello world"
-
+GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_DESCRIPTION = "Another description"
 
 
 
@@ -20,7 +20,7 @@ with open(GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE, 'a') as fd:
         <div class="card blue-grey darken-1">
           <div class="card-content white-text">
             <span class="card-title">{GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_TITLE}</span>
-            <p></p>
+            <p>{GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_DESCRIPTION}</p>
           </div>
           <div class="card-action">
               <md-filled-button>Ready Fredy</md-filled-button>
