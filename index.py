df8e7e9db487987c02433bbd58610ae790b2732d
senalbulumulle <senal.bulumulle@gmail.com>
2024-08-15 07:57:36 -0700

    Gotta comment...

--- index.py
+++ index.py
@@ -1,11 +1,21 @@
+
+'''
+This is where we import the modules
+'''
+
 from flask import Flask, redirect,url_for,render_template                       ## Import the flask library
 import os                                                                       ## Import the standard OS library
 import json                                                                     ## Import the json library
 
 app = Flask(__name__)
 
-from routes import main_routes
 
+
+'''
+This is where we initialize the routes via subjects generation
+via routes.py
+'''
+from routes import main_routes
 app.register_blueprint(main_routes)
 
 
