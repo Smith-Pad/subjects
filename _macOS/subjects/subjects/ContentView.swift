a41731120f496aa302d42f30791a92b73bba3197
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-14 09:29:12 -0500

    Prepare for macOS native support for Subjects

---
+++ _macOS/subjects/subjects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  subjects
+//
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ _macOS/subjects/subjects/subjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  subjectsApp.swift
+//  subjects
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct subjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
