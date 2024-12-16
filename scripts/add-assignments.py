c8d03fa4f89aef148247de437278786b2850c286
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-16 12:24:24 -0500

    did some changes to add-assignments.py [part-1]

--- scripts/add-assignments.py
+++ scripts/add-assignments.py
@@ -1,31 +1,43 @@
+'''
+CLI VERSION
+'''
+
+
 import os
 
-## Variables for adding the Assignment Cards that are considered as latest assignments on the home screen.
+## Variables for adding the Assignment Cards that are considered as latest assignments on the home
 GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE = "../templates/_latest.assignments.cards.html"
-GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_TITLE = "Hello world"
-GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_DESCRIPTION = "Another description"
 
 
 
 
+TITLE_OF_LESSON = input("Create the title of the lesson:        ")
+DESCRIPTION_OF_LESSON = input("Create the description of the lesson:        ")
 
 
-with open(GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE, 'a') as fd:
-    fd.write(f'\n\n\n\n\n')
+# with open(GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE, 'a') as fd:
+#     fd.write(f'\n\n\n\n\n')
 
 
-with open(GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE, 'a') as fd:
-    fd.write(f'''
-      <div class="col s12 m3">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">{GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_TITLE}</span>
-            <p>{GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_CARD_DESCRIPTION}</p>
-          </div>
-          <div class="card-action">
-              <md-filled-button>Ready Fredy</md-filled-button>
-          </div>
-        </div>
-      </div>
+# with open(GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE, 'a') as fd:
+#     fd.write(f'''
+#       <div class="col s12 m3">
+#         <div class="card blue-grey darken-1">
+#           <div class="card-content white-text">
+#             <span class="card-title">{TITLE_OF_LESSON}</span>
+#             <p>{DESCRIPTION_OF_LESSON}</p>
+#           </div>
+#           <div class="card-action">
+#               <md-filled-button>Ready Fredy</md-filled-button>
+#           </div>
+#         </div>
+#       </div>
     
-    ''')
+#     ''')
+
+
+
+
+'''
+In this section, this is where we are able to 
+'''
