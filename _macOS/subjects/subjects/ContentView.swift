c1813255aa5dfcc29e0aa8ee529947b49682155d
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-20 15:09:31 -0500

    that might work

--- _macOS/subjects/subjects/ContentView.swift
+++ _macOS/subjects/subjects/ContentView.swift
@@ -9,16 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack
+        {
+            // text should be hello world
+            Text("Hello World!")
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
