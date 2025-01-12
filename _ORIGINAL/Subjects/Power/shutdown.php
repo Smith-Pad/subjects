e3436a8459769daa03b6403a958886893ffd7da4
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-12 00:39:32 -0500

    Imported the subjects original -> ported from original Smith-Pad

---
+++ _ORIGINAL/Subjects/Power/shutdown.php
@@ -0,0 +1,64 @@
+<?php
+exec("sudo halt");
+?>
+
+<link rel="stylesheet" href="../src/Custom_Tags.css" type="text/css">
+<link rel="stylesheet" href="../src/position-text.css" type="text/css">
+<link rel="stylesheet" href="../src/UI.css" type="text/css">
+<link rel="stylesheet" href="../src/FOIL-Bar.css" type="text/css">
+
+<style>
+
+    body{
+        background-color:black;
+        color:white;
+    }
+
+
+</style>
+
+
+
+<center>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+
+
+<fontsize90> Shutting Down.... </fontsize90> 
+
+</center>
---
+++ _ORIGINAL/Subjects/README.md
@@ -0,0 +1,3 @@
+<img src="Smith-Pad-Logo.png"></img>
+<h1> Smith-Pad Revision </h1>
+<p> Subjects is the modfied version of the thing </b>
