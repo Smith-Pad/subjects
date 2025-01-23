6ce021f3b6214011f85c5b9d64590177c24d89d9
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-23 09:35:11 -0500

    cleanup

--- convert-space-to-underscore.py
+++
@@ -1,9 +0,0 @@
-# Prompt the user to enter something
-user_input = input("Please enter something: ")
-
-# Create a new variable that replaces spaces with underscores
-underscored_input = user_input.replace(' ', '_')
-
-# Print the result
-print("Original input:", user_input)
-print("Modified input:", underscored_input)
