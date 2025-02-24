b1731185ad977c3684ce698ccb0b24fca605ab3c
senalbulumulle <senal.bulumulle@gmail.com>
2025-02-24 11:37:37 -0500

    formatted code

--- static/FOIL-time.js
+++ static/FOIL-time.js
@@ -4,50 +4,50 @@
 // Make a Function called "time()".
 function time() {
 
-    // Make a Variable called "DATE" to call the "new Date();" object.
-    var DATE = new Date();
-  
-    // Make a Variable for Hours, Minutes, and AM or PM.
-    var HOURS = DATE.getHours() , MINUTES = DATE.getMinutes(), INDICATOR;
+        // Make a Variable called "DATE" to call the "new Date();" object.
+        var DATE = new Date();
 
-    // Make a if-else statement for the time.
-    if (HOURS == 0) {
-      INDICATOR = " am";
-      HOURS = 12;
-    }
+        // Make a Variable for Hours, Minutes, and AM or PM.
+        var HOURS = DATE.getHours(), MINUTES = DATE.getMinutes(), INDICATOR;
 
-    else if (HOURS < 12) {
-      INDICATOR = " am";
-    } 
+        // Make a if-else statement for the time.
+        if (HOURS == 0) {
+                INDICATOR = " am";
+                HOURS = 12;
+        }
 
-    else if (HOURS == 12) {
-      INDICATOR = " pm";
-    } 
+        else if (HOURS < 12) {
+                INDICATOR = " am";
+        }
 
-    else if (HOURS > 12) {
-      INDICATOR = " pm"; 
-      HOURS -= 12;
-    }
-  
-    if (MINUTES <= 9) MINUTES = "0" + MINUTES;
+        else if (HOURS == 12) {
+                INDICATOR = " pm";
+        }
 
+        else if (HOURS > 12) {
+                INDICATOR = " pm";
+                HOURS -= 12;
+        }
 
-    // Make a Variable called "PRINT".
-    var PRINT = "" + HOURS + ":" + MINUTES + INDICATOR + "";
-
-    // Print out the time.
-    document.getElementById("time_service").innerHTML = PRINT;
-  }
-  
-    // Call the "time()" function.
-   time();
-  
-   // Use "SetInterval" to update the clock per 1 second.
-   setInterval(time, 1000);
+        if (MINUTES <= 9) MINUTES = "0" + MINUTES;
 
 
-   // Use "console.log" for the "time()" function.
-   console.log(time + "*******************IT IS WORKING*******************************");
+        // Make a Variable called "PRINT".
+        var PRINT = "" + HOURS + ":" + MINUTES + INDICATOR + "";
 
-   // Use "SetInterval" for the console.log for the "time()" function.
-   setInterval(console.log(time));
+        // Print out the time.
+        document.getElementById("time_service").innerHTML = PRINT;
+}
+
+// Call the "time()" function.
+time();
+
+// Use "SetInterval" to update the clock per 1 second.
+setInterval(time, 1000);
+
+
+// Use "console.log" for the "time()" function.
+console.log(time + "*******************IT IS WORKING*******************************");
+
+// Use "SetInterval" for the console.log for the "time()" function.
+setInterval(console.log(time));
