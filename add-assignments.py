543c3581d0fa49087fb6d1892fe4b6f2a0d8f55b
senalbulumulle <senal.bulumulle@gmail.com>
2025-03-23 13:26:29 -0400

    mmm

--- add-assignments.py
+++ add-assignments.py
@@ -12,19 +12,9 @@ os.system("touch routes.py")
 
 
 GENERATION_TEST_ADD_LATEST_ASSIGNMENTS_CARDS_SOURCE = "templates/latest-assignments-cards.html"
-
-
 GENERATION_ROUTES_ROUTING = "routes.py"
-
-
-
 TITLE_OF_LESSON = input("Create the title of the lesson:        ")
-
-
 TRANSLATE_TITLE_OF_LESSON_TO_UNDERSCORES = TITLE_OF_LESSON.replace(" ", "_")
-
-
-
 DESCRIPTION_OF_LESSON = input("Create the description of the lesson:         ")
 
 
