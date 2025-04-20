4f08168bd7bb255df84aceecfeae64de3a648aa8
senalbulumulle <senal.bulumulle@gmail.com>
2025-04-20 06:45:45 +0530

    Added file: [activate-cards.sh]

---
+++ activate-cards.sh
@@ -0,0 +1,8 @@
+## activate-cards.sh
+## This script allows the abliity to activate the cards, which are for the latest assignments cards
+## as well as the past-assignments-cards.
+
+
+cd templates/
+touch latest-assignments-cards.html
+touch past-assignments-cards.html
