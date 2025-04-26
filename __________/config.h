dab8a6e4b4eedd6fc8d744ec1ad802f5b2745343
senalbulumulle <senal.bulumulle@gmail.com>
2025-04-26 09:42:08 -0400

    clean

---
+++ __________/config.h
@@ -0,0 +1,19 @@
+/**
+ * config.h
+ *
+ *
+ */
+
+// #ifdef defined(__MACOSX)
+//     #include < Foundation/Foundation.h>
+//     #include < CoreServices/Kernel.h>
+//     #include < CoreServices/SysUTI.h>
+//     #include < Carbon/Carbon.h>
+//     #include < Carbon/IOContext.h>
+// #endif
+
+#pragma once
+
+
+static char hello = "hello";
+
