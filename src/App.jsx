2a5750f601737d0f7f4a062578a70494b9115abd
senalbulumulle <senal.bulumulle>
2025-08-09 02:25:47 -0400

    Call the main-menu class

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import AssignmentList from './AssignmentList.jsx'
+import './subjects.css'
 
 function App() {
         const [count, setCount] = useState(0)
 
         return (
                 <>
-                        Subjects
+                        <body className="subjects-main-menu-global">
+                                Subjects
+                                <ul></ul>
+                                <AssignmentList />
+                        </body>
                 </>
         )
 }
