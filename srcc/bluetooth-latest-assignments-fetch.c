3112b5c066ff3ecdba5eaf387d9ad9f5d99a4bf8
senalbulumulle <senal.bulumulle>
2025-08-09 15:43:05 -0400

    Add: [srcc/bluetooth-latest-assignments-fetch.c]

---
+++ srcc/bluetooth-latest-assignments-fetch.c
@@ -0,0 +1,15 @@
+// bluetooth-latest-assignments-fetch.c
+
+// this program allows the ability in the backend to fetch latest assignments
+// from proxmity between the device and the para's device or local bluetooth
+// device server.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+        printf("bluetooth fetch assignments");
+}
