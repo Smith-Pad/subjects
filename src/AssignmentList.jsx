bf13e65cf3c1501aee4f7f6e0e5e6abe8cbfaa59
senalbulumulle <senal.bulumulle>
2025-08-09 07:27:17 -0400

    Addition: Added AssignmentList.jsx

---
+++ src/AssignmentList.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function AssignmentList() {
+        return (
+                <>
+                        Assignment List
+                </>
+        )
+}
+
+export default AssignmentList;
