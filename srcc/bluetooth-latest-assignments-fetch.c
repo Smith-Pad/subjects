758f59c2b1ff7748ecca292c19e8ce27c24f0d24
senalbulumulle <senal.bulumulle>
2025-08-10 14:36:42 -0400

    updated [bluetooth-latest-assignments-fetch.c]

--- srcc/bluetooth-latest-assignments-fetch.c
+++ srcc/bluetooth-latest-assignments-fetch.c
@@ -2,12 +2,26 @@
 
 // this program allows the ability in the backend to fetch latest assignments
 // from proxmity between the device and the para's device or local bluetooth
-// device server.
+// device server. 
 
 
 #include <stdio.h>
 #include <stdlib.h>
+// import bluetooth library from buildroot linux
+// #include <bluetooth/bluetooth.h>
+// #include <bluetooth/hci.h>
+// #include <bluetooth/hci_lib.h>
+// #include <bluetooth/hci_conn.h>
+// #include <bluetooth/hci_dev.h>
+// #include <bluetooth/hci_err.h>
+// #include <bluetooth/hci_core.h>
 
+
+// this is a function which is used to fetch the latest assignments: 
+void fetch_assignments()
+{
+        
+}
 
 int main()
 {
